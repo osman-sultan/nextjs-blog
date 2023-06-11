d9218acac7cc57c48e462ed73a4b5b1ba50db2f5
Osman Sultan <o.sultan2811@gmail.com>
2023-06-11 01:41:16 -0400

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts.js";
+import utilStyles from "../styles/utils.module.css";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi im Osman! Welcome to my simple blog :). I'm a third year student at
+          the University of Toronto studying Industrial Engineering. I like
+          machine learning and web dev - check me out{" "}
+          <a href="https://github.com/osman-sultan" target="_blank">
+            here
+          </a>
+          .
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
